557e0cd7c4562b6f79a19f8c22b6feeb1dd7b1e9
FelipeAlbertim <felipe.albertim@sptech.school>
2025-04-12 03:39:02 -0300

    um Configurando ambientes de desenvolvemento e produção de aplicação, e manual de como usar nossa api com o banco de dados

--- src/test/java/com/pizzaria/backendpizzaria/BackendPizzariaApplicationTests.java
+++ src/test/java/com/pizzaria/backendpizzaria/BackendPizzariaApplicationTests.java
@@ -2,12 +2,12 @@ package com.pizzaria.backendpizzaria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BackendPizzariaApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
 
-@SpringBootTest
-class BackendPizzariaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+// }
