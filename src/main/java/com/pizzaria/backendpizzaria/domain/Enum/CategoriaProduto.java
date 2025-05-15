bfe25625f992265713545933106467e0a5422d87
FelipeAlbertim <felipe.albertim@sptech.school>
2025-05-15 09:27:35 -0300

    Trocando tipos de bebida por BEBIDAS

--- src/main/java/com/pizzaria/backendpizzaria/domain/Enum/CategoriaProduto.java
+++ src/main/java/com/pizzaria/backendpizzaria/domain/Enum/CategoriaProduto.java
@@ -1,14 +1,11 @@
 package com.pizzaria.backendpizzaria.domain.Enum;
 
 public enum CategoriaProduto {
-    REFRIGERANTE,
-    SUCO,
-    CERVEJA,
-    AGUA,
     PIZZA,
     PORCAO,
     SOBREMESA,
     PIZZA_DOCE,
     ESFIHA,
-    ESFIHA_DOCE
+    ESFIHA_DOCE,
+    BEBIDAS
 }
